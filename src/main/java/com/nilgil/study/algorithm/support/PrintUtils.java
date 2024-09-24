4724bf3b0d97290edbc3b24eccd9d68ec9e58e61
nilgil <nilgil183@gmail.com>
2024-09-25 03:55:52 +0900

    매트릭스 콘솔 출력 개선

--- src/main/java/com/nilgil/study/algorithm/support/PrintUtils.java
+++ src/main/java/com/nilgil/study/algorithm/support/PrintUtils.java
@@ -5,11 +5,22 @@ import java.util.List;
 public class PrintUtils {
 
     public static void printIntMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int col : row) {
-                System.out.print(col + " ");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == 0 && j == 0) {
+                    System.out.print("    ");
+                } else if (i == 0) {
+                    System.out.print(j + " ");
+                } else if (j == 0) {
+                    System.out.print(i + " | ");
+                } else {
+                    System.out.print(matrix[i][j] + " ");
+                }
             }
             System.out.println();
+            if (i == 0) {
+                System.out.println("  "+"-".repeat(matrix.length * 2));
+            }
         }
     }
 
