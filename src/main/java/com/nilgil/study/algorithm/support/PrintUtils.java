2aa078f3c14499f9768bfb5ff280474008b85fbf
nilgil <nilgil183@gmail.com>
2024-09-26 06:42:26 +0900

    util 수정

--- src/main/java/com/nilgil/study/algorithm/support/PrintUtils.java
+++ src/main/java/com/nilgil/study/algorithm/support/PrintUtils.java
@@ -5,22 +5,11 @@ import java.util.List;
 public class PrintUtils {
 
     public static void printIntMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                if (i == 0 && j == 0) {
-                    System.out.print("    ");
-                } else if (i == 0) {
-                    System.out.print(j + " ");
-                } else if (j == 0) {
-                    System.out.print(i + " | ");
-                } else {
-                    System.out.print(matrix[i][j] + " ");
-                }
+        for (int[] cells : matrix) {
+            for (int cell : cells) {
+                System.out.print(cell + " ");
             }
             System.out.println();
-            if (i == 0) {
-                System.out.println("  "+"-".repeat(matrix.length * 2));
-            }
         }
     }
 
