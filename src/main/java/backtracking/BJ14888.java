f93731e24b56ed965558990629d5cc4ac2145b54
nilgil <nilgil183@gmail.com>
2024-10-03 23:58:30 +0900

    백트래킹 - 백준 14888

---
+++ src/main/java/backtracking/BJ14888.java
@@ -0,0 +1,76 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ14888 {
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        nums = new int[n];
+        calculated = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < operations.length; i++) {
+            operations[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static int n;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+    static int[] nums, calculated;
+    static int[] operations = new int[4];
+
+    private static void recursive(int depth) {
+        if (depth == n) {
+            max = Math.max(max, calculated[n - 1]);
+            min = Math.min(min, calculated[n - 1]);
+        } else {
+            for (int i = 0; i < 4; i++) {
+                if (hasOperator(i)) {
+                    continue;
+                }
+                calculate(depth, i);
+                operations[i]--;
+
+                recursive(depth + 1);
+
+                calculated[depth] = 0;
+                operations[i]++;
+            }
+        }
+    }
+
+    private static boolean hasOperator(int i) {
+        return operations[i] <= 0;
+    }
+
+    private static void calculate(int depth, int i) {
+        if (i == 0) {
+            calculated[depth] = calculated[depth - 1] + nums[depth];
+        } else if (i == 1) {
+            calculated[depth] = calculated[depth - 1] - nums[depth];
+        } else if (i == 2) {
+            calculated[depth] = calculated[depth - 1] * nums[depth];
+        } else {
+            calculated[depth] = calculated[depth - 1] / nums[depth];
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+
+        calculated[0] = nums[0];
+        recursive(1);
+
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
---
+++ src/main/java/backtracking/BJ14888_BEST.java
@@ -0,0 +1,65 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ14888_BEST {
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < operations.length; i++) {
+            operations[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static int n;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+    static int[] nums;
+    static int[] operations = new int[4];
+
+    private static void recursive(int depth, int result) {
+        if (depth == n) {
+            max = Math.max(max, result);
+            min = Math.min(min, result);
+        } else {
+            for (int i = 0; i < 4; i++) {
+                if (1 <= operations[i]) {
+                    operations[i]--;
+                    recursive(depth + 1, calculate(result, nums[depth], i));
+                    operations[i]++;
+                }
+            }
+        }
+    }
+
+    private static int calculate(int operand1, int operand2, int operation) {
+        if (operation == 0) {
+            return operand1 + operand2;
+        } else if (operation == 1) {
+            return operand1 - operand2;
+        } else if (operation == 2) {
+            return operand1 * operand2;
+        } else {
+            return operand1 / operand2;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+
+        recursive(1, nums[0]);
+
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
