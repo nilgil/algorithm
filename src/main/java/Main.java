9fc51f4c2d8566d0c4fd907b32810f915a77630c
nilgil <nilgil183@gmail.com>
2024-10-03 21:00:11 +0900

    update default template

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,19 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    static StringBuilder sb = new StringBuilder();
+
+    private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
 
     }
 }
