a562eb881d2dd8a0c98f60fb4016e885ad2c2a1f
nilgil <nilgil183@gmail.com>
2024-10-08 23:09:41 +0900

    그리디 S4 - 백준 16200

---
+++ src/main/java/greedy/BJ16200_S4.java
@@ -0,0 +1,38 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ16200_S4 {
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        limits = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            limits[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static int n;
+    static int[] limits;
+
+    public static void main(String[] args) throws Exception {
+        input();
+
+        Arrays.sort(limits);
+
+        int count = 0;
+        for (int i = 0; i < n; i += limits[i]) {
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
