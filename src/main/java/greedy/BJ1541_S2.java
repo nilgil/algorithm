9336cd4bb8b81e1d3fb4157291302714c03ba956
nilgil <nilgil183@gmail.com>
2024-10-13 18:04:40 +0900

    그리디/파싱 S2 - 백준 1541

---
+++ src/main/java/greedy/BJ1541_S2.java
@@ -0,0 +1,28 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ1541_S2 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+
+        int result = sum(st.nextToken());
+        while (st.hasMoreTokens()) {
+            result -= sum(st.nextToken());
+        }
+        System.out.println(result);
+    }
+
+    static int sum(String expression) {
+        StringTokenizer st = new StringTokenizer(expression, "+");
+        int sum = 0;
+        while (st.hasMoreTokens()) {
+            sum += Integer.parseInt(st.nextToken());
+        }
+        return sum;
+    }
+}
