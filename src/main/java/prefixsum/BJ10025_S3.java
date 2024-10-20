3a3e5f97bb3d34711c87b2e2db08001e2be2c141
nilgil <nilgil183@gmail.com>
2024-10-20 18:26:11 +0900

    누적합 S3 - 백준 10025

---
+++ src/main/java/prefixsum/BJ10025_S3.java
@@ -0,0 +1,41 @@
+package prefixsum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ10025_S3 {
+
+	static int n, k, windowSize;
+	static int[] ices = new int[1_000_001];
+	static int maxSum = Integer.MIN_VALUE;
+
+	public static void main(String[] args) throws Exception {
+		input();
+		int currentSum = 0;
+		for (int i = 0; i <= 1_000_000; i++) {
+			if (windowSize <= i) {
+				currentSum -= ices[i - windowSize];
+			}
+			currentSum += ices[i];
+			maxSum = Math.max(maxSum, currentSum);
+		}
+		System.out.println(maxSum);
+	}
+
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		windowSize = k * 2 + 1;
+
+		while (n-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int g = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			ices[x] = g;
+		}
+	}
+}
